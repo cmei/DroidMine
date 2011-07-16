40cd64b4d3389f22cab5c88b9718c031ed7e8ecd
unknown <Charlie@.(none)>
2011-07-16 00:08:08 -0700

    Initial commit.

---
+++ src/org/uwaterloo/DroidMine/Main.java
@@ -0,0 +1,12 @@
+package org.uwaterloo.DroidMine;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.main);
+	}
+}
